470f2a49ee85f8aea32457a3e03eee57f505ab75
Ooirahul <rahulsharmaa2000@gmail.com>
2024-06-27 19:17:57 +0530

    Add project files.

---
+++ Divyesh_Sir/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Divyesh_Sir.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
